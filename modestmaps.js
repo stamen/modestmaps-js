2deb098de6d5f6fb43965ba3b368c6b6986fc227
tom.carden <tom.carden@a23cadb0-8de1-11de-82e5-a1837a67dc72>
2010-06-02 03:31:16 +0000

    added distance and interpolation functions
    git-svn-id: https://modestmaps.googlecode.com/svn/trunk/js@898 a23cadb0-8de1-11de-82e5-a1837a67dc72

--- modestmaps.js
+++ modestmaps.js
@@ -1,5 +1,5 @@
 /*!
- * Modest Maps JS v0.10.2
+ * Modest Maps JS v0.10.3
  * http://modestmaps.com/
  *
  * Copyright (c) 2010 Stamen Design, All Rights Reserved.
@@ -97,7 +97,19 @@ if (!com) {
         y: 0,
         toString: function() {
             return "(" + this.x.toFixed(3) + ", " + this.y.toFixed(3) + ")";
-        }
+        }  
+    };
+    
+    MM.Point.distance = function(p1, p2) {
+        var dx = (p2.x - p1.x);
+        var dy = (p2.y - p1.y);
+        return Math.sqrt(dx*dx + dy*dy);
+    };
+    
+    MM.Point.interpolate = function(p1, p2, t) {
+        var px = p1.x + (p2.x - p1.x) * t;
+        var py = p1.y + (p2.y - p1.y) * t;
+        return new MM.Point(px, py);
     };
     
     MM.Coordinate = function(row, column, zoom) {
@@ -196,6 +208,65 @@ if (!com) {
             return "(" + this.lat.toFixed(3) + ", " + this.lon.toFixed(3) + ")";
         }
     };
+
+    /** 
+     * returns approximate distance between start and end locations
+     *
+     * default unit is meters
+     *
+     * you can specify different units by optionally providing the 
+     * earth's radius in the units you desire
+     * 
+     * Default is 6,378,000 metres, suggested values are:
+     *  + 3963.1 statute miles
+     *  + 3443.9 nautical miles
+     *  + 6378 km
+     * 
+     * see http://jan.ucc.nau.edu/~cvm/latlon_formula.html 
+     */
+    MM.Location.distance = function(l1, l2, r) {
+        if (!r) {
+            // default to meters
+            r = 6378000;
+        }
+        var deg2rad = Math.PI / 180.0,
+            a1 = l1.lat * deg2rad,
+            b1 = l1.lon * deg2rad,
+            a2 = l2.lat * deg2rad,
+            b2 = l2.lon * deg2rad,
+            c = Math.cos(a1)*Math.cos(b1)*Math.cos(a2)*Math.cos(b2),
+            d = Math.cos(a1)*Math.sin(b1)*Math.cos(a2)*Math.sin(b2),
+            e = Math.sin(a1)*Math.sin(a2);
+        return Math.acos(c + d + e) * r;
+    };
+    
+    // interpolates along a great circle, f between 0 and 1
+    // FIXME: could be heavily optimized (lots of trig calls to cache)
+    // FIXME: could be inmproved for calculating a full path
+    MM.Location.interpolate = function(l1, l2, f) {
+        var deg2rad = Math.PI / 180.0,
+            lat1 = l1.lat * deg2rad,
+            lon1 = l1.lon * deg2rad,
+            lat2 = l2.lat * deg2rad,
+            lon2 = l2.lon * deg2rad;
+                        
+        var d = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin((lat1-lat2)/2),2) + Math.cos(lat1)*Math.cos(lat2)*Math.pow(Math.sin((lon1-lon2)/2),2)));
+        var bearing = Math.atan2(Math.sin(lon1-lon2)*Math.cos(lat2), Math.cos(lat1)*Math.sin(lat2)-Math.sin(lat1)*Math.cos(lat2)*Math.cos(lon1-lon2))  / -(Math.PI/180);
+        bearing = bearing < 0 ? 360 + bearing : bearing;
+    
+        var A = Math.sin((1-f)*d)/Math.sin(d);
+        var B = Math.sin(f*d)/Math.sin(d);
+        var x = A*Math.cos(lat1)*Math.cos(lon1) + B*Math.cos(lat2)*Math.cos(lon2);
+        var y = A*Math.cos(lat1)*Math.sin(lon1) + B*Math.cos(lat2)*Math.sin(lon2);
+        var z = A*Math.sin(lat1) + B*Math.sin(lat2);
+
+        var latN = Math.atan2(z,Math.sqrt(Math.pow(x,2)+Math.pow(y,2)));
+        var lonN = Math.atan2(y,x);
+        
+        return new MM.Location(latN/deg2rad, lonN/deg2rad);
+    };
+    
+    /////////////////////////////////
     
     MM.Transformation = function(ax, bx, cx, ay, by, cy) {
         this.ax = ax;
