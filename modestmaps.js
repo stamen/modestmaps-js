db5d70278072d77e253c5c7299b8753269f023fb
Tom MacWright <macwright@gmail.com>
2011-06-01 16:36:57 -0400

    Split handlers into individual bindings for drag, mousewheel, and double-click. This change is backwards-compatible

--- modestmaps.js
+++ modestmaps.js
@@ -534,21 +534,116 @@ if (!com) {
     // Event Handlers
     // --------------
 
+    MM.getMousePoint = function(e, map) {
+        // start with just the mouse (x, y)
+        var point = new MM.Point(e.clientX, e.clientY);
+
+        // correct for scrolled document
+        point.x += document.body.scrollLeft + document.documentElement.scrollLeft;
+        point.y += document.body.scrollTop + document.documentElement.scrollTop;
+
+        // correct for nested offsets in DOM
+        for (var node = map.parent; node; node = node.offsetParent) {
+            point.x -= node.offsetLeft;
+            point.y -= node.offsetTop;
+        }
+        return point;
+    };
+
     // map is optional here, use init if you don't have a map yet
-    MM.MouseHandler = function(map) {
+    MM.MouseWheelHandler = function(map) {
         if (map !== undefined) {
             this.init(map);
         }
     };
 
-    MM.MouseHandler.prototype = {
+    MM.MouseWheelHandler.prototype = {
+        init: function(map) {
+            this.map = map;
+            MM.addEvent(map.parent, 'mousewheel', this.getMouseWheel());
+        },
+
+        mouseWheelHandler: null,
+
+        getMouseWheel: function() {
+            // Ensure that this handler is attached once.
+            if (!this.mouseWheelHandler) {
+                var theHandler = this;
+                var prevTime = new Date().getTime();
+                this.mouseWheelHandler = function(e) {
+
+                    var delta = 0;
+                    if (e.wheelDelta) {
+                        delta = e.wheelDelta;
+                    } else if (e.detail) {
+                        delta = -e.detail;
+                    }
+
+                    // limit mousewheeling to once every 200ms
+                    var timeSince = new Date().getTime() - prevTime;
+
+                    if (Math.abs(delta) > 0 && (timeSince > 200)) {
+                        var point = MM.getMousePoint(e, theHandler.map);
+                        theHandler.map.zoomByAbout(delta > 0 ? 1 : -1, point);
+
+                        prevTime = new Date().getTime();
+                    }
+
+                    // Cancel the event so that the page doesn't scroll
+                    return MM.cancelEvent(e);
+                };
+            }
+            return this.mouseWheelHandler;
+        }
+    };
+
+    // map is optional here, use init if you don't have a map yet
+    MM.DoubleClickHandler = function(map) {
+        if (map !== undefined) {
+            this.init(map);
+        }
+    };
+
+    MM.DoubleClickHandler.prototype = {
 
         // Initialize a MouseHandler on a map by adding its events.
         init: function(map) {
             this.map = map;
             MM.addEvent(map.parent, 'dblclick', this.getDoubleClick());
+        },
+
+        doubleClickHandler: null,
+
+        getDoubleClick: function() {
+            // Ensure that this handler is attached once.
+            if (!this.doubleClickHandler) {
+                var theHandler = this;
+                this.doubleClickHandler = function(e) {
+                    // Get the point on the map that was double-clicked
+                    var point = MM.getMousePoint(e, theHandler.map);
+
+                    // use shift-double-click to zoom out
+                    theHandler.map.zoomByAbout(e.shiftKey ? -1 : 1, point);
+
+                    return MM.cancelEvent(e);
+                };
+            }
+            return this.doubleClickHandler;
+        }
+    };
+
+    // map is optional here, use init if you don't have a map yet
+    MM.DragHandler = function(map) {
+        if (map !== undefined) {
+            this.init(map);
+        }
+    };
+
+    MM.DragHandler.prototype = {
+
+        init: function(map) {
+            this.map = map;
             MM.addEvent(map.parent, 'mousedown', this.getMouseDown());
-            MM.addEvent(map.parent, 'mousewheel', this.getMouseWheel());
         },
 
         mouseDownHandler: null,
@@ -580,7 +675,9 @@ if (!com) {
                 this.mouseMoveHandler = function(e) {
 
                     if (theHandler.prevMouse) {
-                        theHandler.map.panBy(e.clientX - theHandler.prevMouse.x, e.clientY - theHandler.prevMouse.y);
+                        theHandler.map.panBy(
+                            e.clientX - theHandler.prevMouse.x,
+                            e.clientY - theHandler.prevMouse.y);
                         theHandler.prevMouse.x = e.clientX;
                         theHandler.prevMouse.y = e.clientY;
                     }
@@ -609,79 +706,24 @@ if (!com) {
                 };
             }
             return this.mouseUpHandler;
-        },
-
-        mouseWheelHandler: null,
-
-        getMouseWheel: function() {
-            // Ensure that this handler is attached once.
-            if (!this.mouseWheelHandler) {
-                var theHandler = this;
-                var prevTime = new Date().getTime();
-                this.mouseWheelHandler = function(e) {
-
-                    var delta = 0;
-                    if (e.wheelDelta) {
-                        delta = e.wheelDelta;
-                    } else if (e.detail) {
-                        delta = -e.detail;
-                    }
-
-                    // limit mousewheeling to once every 200ms
-                    var timeSince = new Date().getTime() - prevTime;
-
-                    if (Math.abs(delta) > 0 && (timeSince > 200)) {
-                        var point = theHandler.getMousePoint(e);
-                        theHandler.map.zoomByAbout(delta > 0 ? 1 : -1, point);
-
-                        prevTime = new Date().getTime();
-                    }
-
-                    // Cancel the event so that the page doesn't scroll
-                    return MM.cancelEvent(e);
-                };
-            }
-            return this.mouseWheelHandler;
-        },
-
-        doubleClickHandler: null,
-
-        getDoubleClick: function() {
-            // Ensure that this handler is attached once.
-            if (!this.doubleClickHandler) {
-                var theHandler = this;
-                this.doubleClickHandler = function(e) {
-                    // Get the point on the map that was double-clicked
-                    var point = theHandler.getMousePoint(e);
-
-                    // use shift-double-click to zoom out
-                    theHandler.map.zoomByAbout(e.shiftKey ? -1 : 1, point);
-
-                    return MM.cancelEvent(e);
-                };
-            }
-            return this.doubleClickHandler;
-        },
-
-        // Get an `MM.Point` object of the mouse's point within the map
-        // from a basic browser event.
-        getMousePoint: function(e) {
-            // start with just the mouse (x, y)
-            var point = new MM.Point(e.clientX, e.clientY);
-
-            // correct for scrolled document
-            point.x += document.body.scrollLeft + document.documentElement.scrollLeft;
-            point.y += document.body.scrollTop + document.documentElement.scrollTop;
-
-            // correct for nested offsets in DOM
-            for(var node = this.map.parent; node; node = node.offsetParent) {
-                point.x -= node.offsetLeft;
-                point.y -= node.offsetTop;
-            }
-
-            return point;
         }
+    };
 
+    // map is optional here, use init if you don't have a map yet
+    MM.MouseHandler = function(map) {
+        if (map !== undefined) {
+            this.init(map);
+        }
+    };
+
+    MM.MouseHandler.prototype = {
+        // Initialize a MouseHandler on a map by adding its events.
+        init: function(map) {
+            this.map = map;
+            new MM.DragHandler(map);
+            new MM.DoubleClickHandler(map);
+            new MM.MouseWheelHandler(map);
+        }
     };
     MM.TouchHandler = function() { };
 
