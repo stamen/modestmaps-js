35c7b242fc53c7dd7c338f0623d5f8a3ceca50f6
Tom MacWright <tom@macwright.org>
2011-08-29 19:08:11 -0400

    Clearing out gh-pages

--- test/point.test.js
+++
@@ -1,9 +0,0 @@
-var path = require('path'),
-    sys = require('sys'),
-    assert = require('assert'),
-    MM = require('../modestmaps.js');
-
-exports['basic point'] = function() {
-    var p = new MM.Point(0, 1);
-    assert.deepEqual(p, { x: 0, y: 1 });
-};
