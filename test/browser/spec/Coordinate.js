23fd234107104b9040399974e512d1dee56b39e0
Tom MacWright <tom@macwright.org>
2011-08-31 19:54:57 -0400

    Adding first round of browser tests, using Jasmine

---
+++ test/browser/spec/Coordinate.js
@@ -0,0 +1,20 @@
+describe("Coordinate", function() {
+  var coordinate;
+
+  beforeEach(function() {
+    coordinate = new com.modestmaps.Coordinate(0, 0, 2);
+  });
+
+  it("generates a key", function() {
+      expect(typeof coordinate.toKey()).toEqual('string');
+  });
+
+  it("can provide a zoomed-in coordinate", function() {
+      expect((coordinate.zoomBy(1)).zoom).toEqual(3);
+  });
+
+  it("can provide a zoomed-out coordinate", function() {
+      expect((coordinate.zoomBy(-1)).zoom).toEqual(1);
+  });
+
+});
