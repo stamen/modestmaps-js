b9cbbd82ce27f75cb81c2260252b6c44bdc16334
Tom MacWright <tom@macwright.org>
2011-10-05 10:44:21 -0400

    Adding MouseWheelHandler tests

--- test/browser/lib/happen.js
+++ test/browser/lib/happen.js
@@ -18,7 +18,7 @@
             true, // canBubble
             true, // cancelable
             window, // 'AbstractView'
-            o.clicks || 0, // click count
+            o.detail || 0, // click count
             o.screenX || 0, // screenX
             o.screenY || 0, // screenY
             o.clientX || 0, // clientX
@@ -47,7 +47,7 @@
     h.dblclick = function(x, o) {
         h.once(x, extend(o || {}, {
             type: 'dblclick',
-            clicks: 2
+            detail: 2
         }));
     };
 
