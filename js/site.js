ef9b4de95d894fb581f3c18953bd80fa93cb0303
tristen <tristen@tristen.ca>
2011-11-21 09:02:21 -0500

    A bunch of commits. Themeing, templating, images yadda yadda;

--- js/site.js
+++ js/site.js
@@ -1,9 +1,17 @@
 // Application bootstrap.
 $(window).load(function() {
-    $('<div class="demo"></div>')
-        .insertBefore('div.live')
-        .html($('div.live').text())
-        .map(function(elem) {
-            eval($('script', elem).text());
-        });
+    $('<div class="demo"></div>').prependTo('div.live').html($('div.live').text()).map(function(elem) {
+        eval($('script', elem).text());
+    });
+
+    $('a.code').click(function (e) {
+        e.preventDefault();
+        if ($(this).hasClass('active')) {
+            $('.highlight').removeClass('active');
+            $(this).removeClass('active');
+        } else {
+            $('.highlight').addClass('active');
+            $(this).addClass('active');
+        }
+    });
 });
