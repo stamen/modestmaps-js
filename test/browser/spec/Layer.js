952e556e5f6bcb3038f6b32be66ae1a4b7eca063
Tom MacWright <tom@macwright.org>
2011-12-07 14:18:49 -0500

    Adding stub tests for layer instance methods

---
+++ test/browser/spec/Layer.js
@@ -0,0 +1,12 @@
+describe('Layer', function() {
+    // Currently not testing subdomain-based templatedmapprovider, since
+    // the implementation should be kind of undefined.
+    it('layer can be created and destroyed', function() {
+        var p = new MM.TemplatedMapProvider(
+            'http://{S}.tile.openstreetmap.org/{Z}/{X}/{Y}.png', ['a']);
+        var l = new MM.Layer(p);
+
+        l.destroy();
+        expect(l.map).toEqual(null);
+    });
+});
