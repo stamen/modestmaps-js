8f1eca081dfec76945c77539ef25b0e3bd922c0d
Michal Migurski <mike@stamen.com>
2012-02-17 23:47:23 -0800

    Unrolled various templates and includes to plain old HTML/CSS/JS

--- _includes/js/site.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    // Evaluate example code and run
-    $('<div class="demo"></div>').prependTo('div.live').html($('div.live').text()).each(function(elem) {
-        eval($('script', elem).text());
-    });
-});
