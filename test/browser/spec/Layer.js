015872a19f61a43d5276a529161698ecc2e62219
Tom MacWright <tom@macwright.org>
2012-06-29 15:29:02 -0400

    Test for requesterror parameters

--- test/browser/spec/Layer.js
+++ test/browser/spec/Layer.js
@@ -9,4 +9,21 @@ describe('Layer', function() {
         l.destroy();
         expect(l.map).toEqual(null);
     });
+
+    // Currently not testing subdomain-based templatedmapprovider, since
+    // the implementation should be kind of undefined.
+    it('causes the map to throw requesterror when things are not accessible', function() {
+        var fourohfour = 'http://fffffffffffffffffffffffffffffffff.org/404.png';
+        var p = new MM.TemplatedLayer(fourohfour);
+
+        p.requestManager.addCallback('requesterror', function(manager, tile, url) {
+            expect(manager).toEqual(p.requestManager);
+            expect(jasmine.isDomNode(tile)).toBeTruthy();
+            expect(url).toBeEqual(fourohfour);
+        });
+
+        var m = new MM.Map(document.createElement('div'), p, { x: 500, y: 500 });
+
+        m.setCenter({ lat: 0, lon: 0 }).setZoom(5);
+    });
 });
